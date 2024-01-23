43629d4c20d14f9099f73e86e9cdb630f3bcc314
Suyash Purwar <purwarsuyash06@gmail.com>
2024-01-23 08:00:22 +0530

    feat: add script to generate mazes.json

--- src/scripts/generate-mazes.js
+++ src/scripts/generate-mazes.js
@@ -1,10 +1,25 @@
 import mazeGeneration from 'maze-generation';
-import fs from 'node:fs/promises';
+import fs from 'node:fs';
 
-function createMazes(mazeCount, width, height) {
+function createMazes(mazeCount, mazeWidth, mazeHeight) {
+    const mazes = {};
 
+    for (let i = 1; i <= mazeWidth; i++) {
+        mazes[i] = mazeGeneration({
+            width: mazeWidth,
+            height: mazeHeight,
+            seed: Math.random() * 987654
+        }).toString().split("\n");
+    }
+
+    return mazes;
 }
 
-function writeMazesJSON(json) {
-    
+function writeMazesJSON(mazes) {
+    const mazesJSON = JSON.stringify(mazes);
+
+    fs.writeFileSync('src/data/mazes.json', mazesJSON);
 }
+
+const mazes = createMazes(20, 20, 30);
+writeMazesJSON(mazes);
